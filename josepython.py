c9141f8e0c4d1d77425dab2964815b882775cb91
victorbrittoferreira <victorbrittoferreira@gmail.com>
2021-07-11 20:54:35 -0300

    final assessment

--- josepython.py
+++ josepython.py
@@ -263,6 +263,9 @@ my_list1 = [1,2,3]
 #
 #print( my_list1)
 
+
+
+
 #38. List Comprehensions in Python
 
 #my_string = 'hello'
@@ -311,7 +314,7 @@ my_list1 = [1,2,3]
 #print(result)
 
 #mylist = []
-#
+## NESTED FOR
 #for x in [2, 4 ,6]:
 #    for y in [1, 10, 100]:
 #        mylist.append((x * y))
@@ -324,4 +327,67 @@ my_list1 = [1,2,3]
 #
 #print(mylist)
 
+
+
 # Statements Assessment Test
+
+#1 Use for, .split(), and if to create a Statement that will 
+# print out words that start with 's':
+
+#st = 'Print only the words that start with s in this sentence'
+
+#for word in st.split(' '):
+#    #print(len(word))
+#    if word[0] == 's' and len(word) != 1:
+#        print(word)
+#    else:
+#        pass
+         
+#2 Use range() to print all the even numbers from 0 to 10.
+#x = [num for num in range(0, 10) if num %2 == 0 ]
+#print(x)
+
+#y = []
+#for num in range(0, 10):
+#    if num % 2 == 0:
+#        y.append(num)
+#print(y)
+
+#3 Use a List Comprehension to create a list of all numbers
+# between 1 and 50 that are divisible by 3.
+#x = [num for num in range(1, 50) if num %3 == 0 ]
+#print(x)
+
+#4 Go through the string below and if the length of a word
+# is even print "even!"
+
+#st = 'Print every word in this sentence that has an even number of letters'
+#
+#for word in st.split(' '):
+#    if len(word) %2 == 0:
+#        #print('Even {}'.format(word))
+#        print(f'"{word}" is even ')
+#    else:
+#        pass
+
+#5 Write a program that prints the integers from 1 to 100. 
+# But for multiples of three print "Fizz" instead of the number, 
+# and for the multiples of five print "Buzz". For numbers which 
+# are multiples of both three and five print "FizzBuzz".
+
+#for num in range(1, 100):
+#    if num % 2 == 0 and num %3 == 0:
+#        print(f'{num} FizzBuzz')
+#    elif num %3 == 0:
+#        print(f'{num} Fizz')
+#    elif num %5 == 0:
+#        print(f'{num} Buzz')
+    
+
+#6 Use List Comprehension to create a list of the first letters
+# of every word in the string below:
+
+#st = 'Create a list of the first letters of every word in this string'
+#
+#x = [word[0] for word in st.split(' ')]
+#print(x)

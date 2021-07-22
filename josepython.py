f21de0b5e0e4563234580ead9e19c708d3a8c213
victorbrittoferreira <victorbrittoferreira@gmail.com>
2021-07-21 21:37:29 -0300

    tupple functions

--- josepython.py
+++ josepython.py
@@ -454,6 +454,27 @@ my_list1 = [1,2,3]
 #    #print(mark)
 #    print(price+ (0.1*price))
 
-        
+work_hours = [('Abby',100), ('Billy',4000), ('Cassie', 800)]
+
+def employee_check (work_hours):
+    
+    current_max = 0
+    employee_of_month = ''
+    
+    for employee, hours in work_hours:
+        if hours > current_max:
+            current_max = hours
+            employee_of_month = employee
+        else:
+            pass
+    return employee_of_month, current_max
+
+print(employee_check(work_hours))
+
+result = (employee_check(work_hours))
+employee, hours = (employee_check(work_hours))
+
+print(employee)
+
 
 

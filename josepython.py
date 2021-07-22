5e0c5db50cf6234d54ba0b271c79517d11eb9c4f
victorbrittoferreira <victorbrittoferreira@gmail.com>
2021-07-22 15:18:16 -0300

    warm up 2

--- josepython.py
+++ josepython.py
@@ -504,23 +504,44 @@
               
 # 50 Functions practice Exercises
 
-#LESSER OF TWO EVENS: Write a function that returns 
+# WARM UP
+
+#1 LESSER OF TWO EVENS: Write a function that returns 
 # the lesser of two given numbers if both numbers are 
 # even, but returns the greater if one or both numbers are odd
 
 #lesser_of_two_evens(2,4) --> 2
 #lesser_of_two_evens(2,5) --> 5
 
-def lesser_of_two_evens(a,b):
-    if a % 2 == 0 and b % 2 == 0:
-        if a < b:
-            return a
-        else:
-            pass
+#def lesser_of_two_evens(a,b):
+#    if a % 2 == 0 and b % 2 == 0:
+#        if a < b:
+#            return a
+#        else:
+#            pass
+#    else:
+#        if a > b:
+#            return a
+#        else:
+#            return b
+#        
+#print(lesser_of_two_evens(2,5))
+
+#2 ANIMAL CRACKERS: Write a function takes a two-word string and 
+#returns True if both words begin with same letter
+
+#animal_crackers('Levelheaded Llama') --> True
+#animal_crackers('Crazy Kangaroo') --> False
+
+#x = 'Levelheaded Llama'
+#print(x.split(' '))
+
+def animal_crackers(text):
+
+    words = list(text.split(' '))
+    if words[0][0] == words[1][0]:
+        return True        
     else:
-        if a > b:
-            return a
-        else:
-            return b
-        
-print(lesser_of_two_evens(2,5))
+        return False
+    
+print(animal_crackers('Levelheaded Llama'))

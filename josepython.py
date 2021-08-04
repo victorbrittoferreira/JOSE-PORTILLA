caa39a2286fbd7884854991c85d17d39ccbbe198
victorbrittoferreira <victorbrittoferreira@gmail.com>
2021-08-03 23:56:54 -0300

    lambda 3

--- josepython.py
+++ josepython.py
@@ -770,12 +770,23 @@
 
 ## 55 Lambda Expressions Map and Filter
 
-def square (num):
-    return num**2
-
-my_nums = [1,2,3,4,5]
-
-#for item in map(square, my_nums)
+#def square (num):
+#    return num**2
+#
+#my_nums = [1,2,3,4,5]
+#
+#for item in map(square, my_nums):
 #    print(item)
+#
+#print(list(map(square, my_nums)))
 
-print(list(map(square, my_nums)))
+
+def splicer (mystring):
+    if len (mystring)% 2 == 0:
+        return 'Even'
+    else:
+        return mystring[0]
+    
+names = ['Andy', 'Eve', 'Sally']
+
+print(list(map(splicer, names)))

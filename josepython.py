2d305fbfbbf14390634af0cbe1d592175faaf6d3
victorbrittoferreira <victorbrittoferreira@gmail.com>
2021-08-05 23:58:48 -0300

    57

--- josepython.py
+++ josepython.py
@@ -789,15 +789,15 @@
 #print(list(map(square, my_nums)))
 
 
-def splicer (mystring):
-    if len (mystring)% 2 == 0:
-        return 'Even'
-    else:
-        return mystring[0]
-    
-names = ['Andy', 'Eve', 'Sally']
- 
-print(list(map(splicer, names)))
+#def splicer (mystring):
+#    if len (mystring)% 2 == 0:
+#        return 'Even'
+#    else:
+#        return mystring[0]
+#    
+#names = ['Andy', 'Eve', 'Sally']
+# 
+#print(list(map(splicer, names)))
 
 ## filter function
 
@@ -811,26 +811,24 @@ print(list(map(splicer, names)))
 #def check_even(num):
 #    return num % 2 == 0
 #
-#nums = [0,1,2,3,4,5,6,7,8,9,10]
+#my_nums = [0,1,2,3,4,5,6,7,8,9,10]
 #
-#filter(check_even,nums)
+##filtred = []
+##for n in filter(check_even,my_nums):
+##    filtred.append(n)
+##print(filtred)
 #
-#list(filter(check_even,nums))
-
+#print(list(filter(check_even,my_nums)))
 
 ## lambda expression
 
-#One of Pythons most useful (and for beginners, confusing) tools
-# is the lambda expression. lambda expressions allow us to
-# create "anonymous" functions. This basically means we can 
-# quickly make ad-hoc functions without needing to properly 
-# define a function using def.
+# lambda expressions allow us to create "anonymous" functions.
+# This basically means we can quickly make ad-hoc functions
+# without needing to properly define a function using def.
 #Function objects returned by running lambda expressions work 
 # exactly the same as those created and assigned by defs.
 # There is key difference that makes lambda useful in
 # specialized roles:
-
-#lambda's body is a single expression, not a block of statements.
 
 #    The lambda's body is similar to what we would put in a def
 # body's return statement. We simply type the result as an 
@@ -842,9 +840,10 @@ print(list(map(splicer, names)))
 
 #def square(num):
 #    return num**2
+## bad style to coding
 #def square(num): return num**2
-
-#square(2)
+#
+#print(square(2))
 
 #This is the form a function that a lambda expression intends 
 #to replicate. A lambda expression can then be written as:
@@ -852,22 +851,19 @@ print(list(map(splicer, names)))
 #lambda num: num ** 2
 #square = lambda num: num **2
 #
-#square(2)
+#print(square(2))
 
-#So why would use this? Many function calls need a function
-#passed in, such as map and filter. Often you only need to
-#use the function you are passing in once, so instead of 
-#formally defining it, you just use the lambda expression. 
-#Let's repeat some of the examples from above with a lambda 
-#expression
+#my_nums = [0,1,2,3,4,5,6,7,8,9,10]
+#
+#filtred = []
+#for n in map(lambda num: num ** 2, my_nums):
+#    filtred.append(n)
+#    
+#print(filtred)
 
-#list(map(lambda num: num ** 2, my_nums))
-#
-#[1, 4, 9, 16, 25]
-#
-#list(filter(lambda n: n % 2 == 0,nums))
-#
-#[0, 2, 4, 6, 8, 10]
+#print(list(map(lambda num: num ** 2, my_nums)))
+# 
+#print(list(filter(lambda n: n % 2 == 0,my_nums)))
 
 #Here are a few more examples, keep in mind the more comples a
 #function is, the harder it is to translate into a lambda 
@@ -875,8 +871,9 @@ print(list(map(splicer, names)))
 #the only way) to create the def keyword function.
 
 #Lambda expression for grabbing the first character of a string:
-
-#lambda s: s[0]
+#names = ['Andy', 'Eve', 'Sally']
+#
+#print(list(map(lambda s: s[0], names)))
 #
 #Lambda expression for reversing a string:
 #
@@ -887,8 +884,132 @@ print(list(map(splicer, names)))
 #lambda x,y : x + y
 
 
-#You will find yourself using lambda expressions often with 
-#certain non-built-in libraries, for example the pandas library
-#for data analysis works very well with lambda expressions.
+##56 Nested Statements and Scope
 
+#x = 25
+#
+#def printer():
+#    x = 50
+#    return x
+#
+#print(x)
+#print(printer())
+#
+#3 general rules:
+#
+#   1 Name assignments will create or change local names by default.
+#   2 Name references search (at most) four scopes, these are:
+#        local
+#        enclosing functions
+#        global
+#        built-in
+#   3 Names declared in global and nonlocal statements map assigned
+#   names to enclosing module and function scopes.
+#
+#The statement in 2 above can be defined by the LEGB rule.
+#
+#LEGB Rule:
+#
+#L: Local — Names assigned in any way within a function 
+# (def or lambda), and not declared global in that function.
+#
+#E: Enclosing function locals — Names in the local scope
+# of any and all enclosing functions (def or lambda), from
+# inner to outer.
+#
+#G: Global (module) — Names assigned at the top-level of a
+# module file, or declared global in a def within the file.
+#
+#B: Built-in (Python) — Names preassigned in the built-in
+# names module : open, range, SyntaxError,...
+#
+## Local
+#
+## x is local here:
 
+#f = lambda x:x**2
+#print(f)
+#
+## Enclosing function locals
+#
+#name = 'This is a global name'
+#
+#def greet():
+#    # Enclosing function
+#    #name = 'This is a enclosing string'
+#    
+#    def hello():
+#        # Local
+#        name = 'This a local string'
+#        print('Hello! '+name)
+#    
+#    hello()
+#
+#greet()
+#
+#x = 50
+#
+##Local Variables
+#
+#x = 50
+#
+#def func(x):
+#    print('x is', x)
+#    x = 2
+#    print('Changed local x to', x)
+#
+#func(x)
+#print('x is still', x)
+#
+#If you want to assign a value to a name defined at the top
+# level of the program (i.e. not inside any kind of scope 
+# such as functions or classes), then you have to tell 
+# Python that the name is not local, but it is global. 
+# We do this using the global statement. It is impossible 
+# to assign a value to a variable defined outside a function
+# without the global statement.
+#
+#You can use the values of such variables defined outside the
+# function (assuming there is no variable with the same name
+# within the function). However, this is not encouraged and
+# should be avoided since it becomes unclear to the reader 
+# of the program as to where that variable’s definition is.
+# Using the global statement makes it amply clear that the
+# variable is defined in an outermost block.
+#
+#x = 50
+#
+##def func():
+#    #global x
+#    
+#    # put the statement inside the the func + return statement
+#    # to afect it globally.
+#    ## safer
+#def func(x):
+#    print('This function is now using the global x!')
+#    print(f'Because of global x is: {x}')
+#    x = 200
+#    print(f'Run func(), changed global x to {x}')
+#    return x
+#
+#print('Before calling func(), x is: ', x)
+#func(x)
+#print('Value of x (outside of func()) is: ', x)
+#
+#The global statement is used to declare that x is a global
+# variable - hence, when we assign a value to x inside the
+# function, that change is reflected when we use the value
+# of x in the main block.
+#
+#You can specify more than one global variable using the 
+# same global statement e.g. global x, y, z.
+#
+# One last mention is that you can use the globals()
+# and locals() functions to check what are your current local
+# and global variables.
+#
+#Another thing to keep in mind is that everything in Python
+# is an object! I can assign variables to functions just 
+# like I can with numbers!
+
+## 57

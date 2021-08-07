9322bba2dde02d1525b500597b19318a1cfaabdb
victorbrittoferreira <victorbrittoferreira@gmail.com>
2021-08-07 16:29:39 -0300

    homework mnf 2

--- josepython.py
+++ josepython.py
@@ -1014,8 +1014,8 @@
 
 ## 57 Method n Functions Homework
 
-#Write a function that computes the volume of a sphere
-# given its radius.
+# Write a function that computes the volume of a sphere
+#given its radius.
 
 #def vol(rad):
 #    
@@ -1026,9 +1026,12 @@
 #    
 #print(vol(2))
 
-#Write a function that checks whether a number is in a 
+# Write a function that checks whether a number is in a 
 #given range (inclusive of high and low)
 
 def ran_check(num,low,high):
     
+    return low < num < high
+
+print(ran_check(5,2,7))
     
